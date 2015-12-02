7db78dfd55c9d90895ec27327838ae8f348817a6
GregOSF <gregory.onza@gmail.com>
2015-12-01 23:54:00 -0800

    Fizzbuzz logic and render functions

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function() {
+
+	var buzzController = {
+
+		template: _.template($('#buzzTemplate').html()),
+
+		render: function(buzzRend) {
+
+		}
+
+
+		fizzbuzz: function() {
+			for (var i = 0; i <= 10; i++) {
+				buzzController.render
+					}
+
